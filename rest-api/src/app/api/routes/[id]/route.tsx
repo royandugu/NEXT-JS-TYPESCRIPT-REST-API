fcf8bfa2413209bf5ae546362506f84b465e3d07
royandugu <royandugu99009@gmail.com>
2023-09-12 19:55:10 +0545

    Dynamic routing for API and RD

---
+++ rest-api/src/app/api/routes/[id]/route.tsx
@@ -0,0 +1,3 @@
+// export const UPDATE=async (request:any, {params}):Promise<any>=>{
+//     const {id}=params;
+// }
--- rest-api/src/app/api/routes/route.tsx
+++ rest-api/src/app/api/routes/route.tsx
@@ -9,5 +9,18 @@ export const POST=async (request:any):Promise<any>=>{
     await Connector();
     await Model.create({title:title, todo:todo});
 
-    return NextResponse.json({message:"Data created"},{status:200});
+    return NextResponse.json({message:"Data created"},{status:201});
+}
+
+export const GET=async (request:any):Promise<any>=>{
+    await Connector();
+    const todo=await Model.find({});
+    return NextResponse.json({message:"Data fetched sucesfully", todo:todo},{status:200}) 
+}
+
+export const DELETE=async (request:any):Promise<any>=>{
+    const id=request.nextUrl.searchParams.get("id");
+    await Connector();
+    await Model.findByIdAndDelete(id);
+    return NextResponse.json({message: "Data deleted sucesfully"},{status:200})
 }
